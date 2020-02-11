6bd9308afb92a92c5554cf1f0c2cb74b8b97e05e
Ellin Tj <tjellin@gmail.com>
2020-02-10 20:22:24 -0800

    test

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -1,6 +1,6 @@
   $(document).ready(function(){
     //$('.fixed-action-btn').floatingActionButton();
-
+      // adding test comment
     
     let searchInput = $("#search");
     console.log("searchInput ======" + searchInput);
