9bf629d72a40d8703f193e34ba0cbdac64f62dee
Ellin Tj <tjellin@gmail.com>
2020-02-10 20:31:03 -0800

    added ajax

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -5,8 +5,16 @@
     let searchInput = $("#search");
     console.log("searchInput ======" + searchInput);
 
+    
+    let edamamURL = "https://api.edamam.com/search?q=" + searchInput + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750"
 
-
+    $.ajax({
+      url: edamamURL,
+      method: "GET"
+    }).then( function(response) {
+      console.log("inside edamam's ajax");
+      console.log(response);
+    });
 
 
 
