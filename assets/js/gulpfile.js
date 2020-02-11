7c0eb6d7b48b94ce5dfd09e06645a094bce55eb3
Ellin Tj <tjellin@gmail.com>
2020-02-10 20:13:51 -0800

    making sure the .js can understand jQuery

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -1,6 +1,7 @@
   $(document).ready(function(){
     //$('.fixed-action-btn').floatingActionButton();
 
+    
     let searchInput = $("#search");
     console.log("searchInput ======" + searchInput);
 
