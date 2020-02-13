32ff6a2a00e3eb2a68b6ee3cfda2153b6085d10d
Ellin Tj <tjellin@gmail.com>
2020-02-12 20:27:20 -0800

    got the event listener to work, and the api as well

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -1,13 +1,11 @@
-  $(document).ready(function(){
-    //$('.fixed-action-btn').floatingActionButton();
-    
+$(document).ready(function(){
+  
 
-    $("#search").on("click", function(e) {
-      console.log(e);
-
-      // let searchInput = $("#search").val();
-      let searchInput = "hamburger";
-      console.log("searchInput ======" + searchInput);
+  $(document).on('keypress',function(e) {
+    if(e.which == 13) {
+      event.preventDefault();
+      let searchInput = $("#search").val();
+      console.log("searchInput ====== " + searchInput);
 
       let edamamURL = "https://api.edamam.com/search?q=" + searchInput + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750";
   
@@ -21,23 +19,25 @@
         mapIt(searchInput);
       });
 
-    });
-
-    
-
-    function mapIt (input) {
-      let placeURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + input + "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=YOUR_API_KEY";
-
-      $.ajax( {
-        url: placeURL,
-        method: "GET"
-      }).then (function(response) {
-        console.log("inside Google Place");
-        console.log(response);
-
-
-      });
     }
+    
+  });
+
+      
+
+  function mapIt (input) {
+    let placeURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + input + "&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=YOUR_API_KEY";
+
+    $.ajax( {
+      url: placeURL,
+      method: "GET"
+    }).then (function(response) {
+      console.log("inside Google Place");
+      console.log(response);
+
+
+    });
+  }
 
 
   });
