848fc7aeca2cc7c17621e06b71c41ee9a6059095
Ellin Tj <tjellin@gmail.com>
2020-02-14 15:41:40 -0800

    getting the recipe page better looking!

--- assets/js/chewfile.js
+++ assets/js/chewfile.js
@@ -1,9 +1,28 @@
 let array = JSON.parse(localStorage.getItem("recipe"));
-let $cardContent = $(".card-content")
 
+$(document).ready(function(){
+    console.log("yippie!!");
 
+    for (let i = 0; i<array.length; i++) {
+        let div1 = $("<div>").addClass("col s5");
 
+        let card = $("<div>").addClass("card");
+        card.append($("<div>").addClass("card-image waves-effect waves-block waves-light").append("<img>").addClass("activator").attr("src", ""));
 
+        let cardContent = $("<div>").addClass("card-content");
+        cardContent.append($("<span>").addClass("card-title activator grey-text text-darken-4").text(array[i].label)).append($("<i>").addClass("material-icons right").text("more_vert"));
+        cardContent.append($("<p>"));
+        cardContent.append($("<a>").addClass("theURL").attr("href", array[i].url).text("See complete recipe"));
+        card.append(cardContent);
+
+        let cardReveal = $("<div>").addClass("card-reveal");
+        cardReveal.append($("<span>").addClass("card-title grey-text text-darken-4").text("Recipe").append($("<i>").addClass("material-icons right").text("close")));
+        cardReveal.append($("<p>").text("placeholder"));
+        card.append(cardReveal);
+
+        div1.append(card);
+        $(".row").append(div1);
+    }
 {/* <div class="col s5">
         <div class="card">
             <div class="card-image waves-effect waves-block waves-light">
@@ -19,24 +38,5 @@ let $cardContent = $(".card-content")
             </div>
         </div>
 </div> */}
-
-$(document).ready(function(){
-    console.log("yippie!!");
-
-    for (let i = 0; i<array.length; i++) {
-        let div1 = $("<div>").addClass("col s5");
-
-        let card = $("<div>").addClass("card");
-        card.append($("<div>").addClass("card-image waves-effect waves-block waves-light").append("<img>").addClass("activator").attr("src", ""));
-
-        let cardContent = $("<div>").addClass("card-content");
-        cardContent.append($("<span>").addClass("card-title activator grey-text text-darken-4").text(array[i].label)).append($("<i>").addClass("material-icons right").text("more_vert"));
-        card.append(cardContent);
-
-        div1.append(card);
-
-        $(".row").append(div1);
-    }
-
 
 });
