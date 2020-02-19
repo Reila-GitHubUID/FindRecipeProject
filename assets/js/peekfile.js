8b5a29ec28ac4008655503e56355b5310441eae4
Ellin Tj <tjellin@gmail.com>
2020-02-18 16:16:38 -0800

    adding the 2nd API to the project

--- assets/js/peekfile.js
+++ assets/js/peekfile.js
@@ -1,7 +1,7 @@
-let array = JSON.parse(localStorage.getItem("recipe"));
+// let array = JSON.parse(localStorage.getItem("recipe"));
 
 $(document).ready(function(){
-    console.log("yippie!!");
+    console.log("Picaboooo!!");
 
     for (let i = 0; i<array.length; i++) {
         let div1 = $("<div>").addClass("col s5");
@@ -18,20 +18,6 @@ $(document).ready(function(){
         let cardReveal = $("<div>").addClass("card-reveal");
         cardReveal.append($("<span>").addClass("card-title grey-text text-darken-4").text("Recipe").append($("<i>").addClass("material-icons right").text("close")));
         cardReveal.append($("<span>").text(array[i].ingredients));
-        
-        //----------- EL: attempt to separate ingredients into lines --------//
-        // let newSpan = $("<span>");
-        // const eacher = function(collection) {            
-        //     for (let i = 0; i < collection.length; i++) {
-        //         newSpan.text(collection[i], i, collection);
-        //         cardReveal.append(newSpan);
-
-        //         console.log(collection[i]);
-        //     }
-        // };
-        // eacher(array[i].ingredients);
-        // cardReveal.append(newSpan);
-
         
         cardReveal.append($("<p>").text("placeholder"));
         card.append(cardReveal);
