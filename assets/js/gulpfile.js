e8ffd11278de027a3441190868a5a944724408a5
Ellin Tj <tjellin@gmail.com>
2020-02-18 18:21:11 -0800

    tidying up the codes by creating some functions

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+  let searchInput = $("#search").val();
+
   $(document).on('keypress',function(event) {
     if(event.which == 13) {
-      findRecipe(event);
+      findRecipe(searchInput);
     }
   });
 
   
-  $(".btn-large").click(function(event) {
-    if ($(this).text() === "Dine-In") {
-      if ($("#search").val() === "") {
-        return;
+  $(".btn-large").click(function() {
+    if ($("#search").val() === "") {
+      return;
+    }
+    else {
+      if ($(this).text() === "Dine-In") {        
+        findRecipe(searchInput);       
       }
-      else {
-        findRecipe(event);
-        console.log("findRecipe()");        
+      else if ($(this).text() === "Dine-Out") {
+        mapIt(searchInput);
+        console.log("mapIt(event)");
       }
     }
   });
@@ -21,13 +26,9 @@ $(document).ready(function(){
 });
 
 // AJAX function to find the recipe
-function findRecipe(e) {    
-  event.preventDefault();
-  let searchInput = $("#search").val();
-  console.log("searchInput ====== " + searchInput);
-  console.log(e);
-
-  let edamamURL = "https://api.edamam.com/search?q=" + searchInput + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750";
+function findRecipe(input) {   
+  event.preventDefault();  
+  let edamamURL = "https://api.edamam.com/search?q=" + input + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750";
 
   $.ajax({
     url: edamamURL,
@@ -43,7 +44,6 @@ function findRecipe(e) {
         ingredients: response.hits[i].recipe.ingredientLines,
         url: response.hits[i].recipe.shareAs
       });
-      // console.log(arr[i].label + ", " + arr[i].ingredients + ", " + arr[i].url);
     }
 
     localStorage.setItem("recipe", JSON.stringify(arr));
@@ -67,7 +67,7 @@ function mapIt (input) {
 
 
 
-    $(window).attr("location", "locationPage.html");  // this is to direct users to the recipePage.html
+    $(window).attr("location", "locationPage.html");  // this is to direct users to the locationPage.html
 
   });
 }
