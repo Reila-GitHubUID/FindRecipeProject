262cf9282b74dea0a4a581bc64bec5d9c3b10b70
Ellin Tj <tjellin@gmail.com>
2020-02-19 20:58:22 -0800

    making the workflow better than the previous version

--- assets/js/gulpfile.js
+++ assets/js/gulpfile.js
@@ -1,46 +1,42 @@
 localStorage.setItem("recipe", "");
-let $search = $("#search").val();
 
 $(document).ready(function(){
   $(document).on('keypress',function(event) {
-    event.preventDefault();  
 
-    if ($search === "") {
+    if ($("#search").val() === "") {
       return;
     }
     else {
       if(event.which == 13) {
         findRecipe();
-        $(window).attr("location", "recipePage.html");  // this is to direct users to the recipePage.html
       }
     }
   });
 
   
   $(".btn-large").click(function() {
-    event.preventDefault();  
 
-    if ($search === "") {
+    if ($("#search").val() === "") {
       return;
     }
     else {
       if ($(this).text() === "Dine-In") {  
-        findRecipe(); 
-        $(window).attr("location", "recipePage.html");  // this is to direct users to the recipePage.html
-      
+        findRecipe();       
       }
       else if ($(this).text() === "Dine-Out") {
-        findRecipe();
-        $(window).attr("location", "locationPage.html");  // this is to direct users to the locationPage.html
+        findMap();
       }
     }
   });
 
 });
 
-// AJAX function to find the recipe
+// AJAX function to find the recipe, and redirect users to recipePage.html
 function findRecipe() {   
-  let edamamURL = "https://api.edamam.com/search?q=" + $search + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750";
+  event.preventDefault();
+  console.log('$("#search").val() === ' + $("#search").val());
+
+  let edamamURL = "https://api.edamam.com/search?q=" + $("#search").val() + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750";
 
   $.ajax({
     url: edamamURL,
@@ -53,7 +49,7 @@ function findRecipe() {
       alert ("You've reached your ajax query limit. You won't be able to do any search anymore!");
     }
     else {
-      arr.push(input);
+      arr.push($("#search").val());
       for (let i = 0; i < 10; i++) {
         arr.push({
           label: response.hits[i].recipe.label,
@@ -64,6 +60,42 @@ function findRecipe() {
     }
 
     localStorage.setItem("recipe", JSON.stringify(arr));
+    $(window).attr("location", "recipePage.html");  // this is to direct users to the recipePage.html
+
+
+  });
+
+}
+
+// the function to create the recipe, and redirect users to locationPage.html
+function findMap() {   
+  event.preventDefault();
+
+  let edamamURL = "https://api.edamam.com/search?q=" + $("#search").val() + "&app_id=29622eed&app_key=898538197a883e8c6561872a165ee750";
+
+  $.ajax({
+    url: edamamURL,
+    method: "GET"
+  }).then( function(response) {
+
+    let arr = [];
+    if (response.q === "")
+    {
+      alert ("You've reached your ajax query limit. You won't be able to do any search anymore!");
+    }
+    else {
+      arr.push($("#search").val());
+      for (let i = 0; i < 10; i++) {
+        arr.push({
+          label: response.hits[i].recipe.label,
+          ingredients: response.hits[i].recipe.ingredientLines,
+          url: response.hits[i].recipe.shareAs
+        });
+      }
+    }
+
+    localStorage.setItem("recipe", JSON.stringify(arr));
+    $(window).attr("location", "locationPage.html");  // this is to direct users to the locationPage.html
 
   });
 
